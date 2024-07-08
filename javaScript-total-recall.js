a94740075dcdcbc24884503a59e45fd8110b6d93
stackerd <danni2mar@gmail.com>
2024-07-07 21:36:16 -0400

    isVowel

--- javaScript-total-recall.js
+++ javaScript-total-recall.js
@@ -201,74 +201,109 @@
 
 //H. What's in your closet
 
-const kristynsCloset = [
-    "left shoe",
-    "cowboy boots",
-    "right sock",
-    "Per Scholas hoodie",
-    "green pants",
-    "yellow knit hat",
-    "marshmallow peeps"
-];
+// const kristynsCloset = [
+//     "left shoe",
+//     "cowboy boots",
+//     "right sock",
+//     "Per Scholas hoodie",
+//     "green pants",
+//     "yellow knit hat",
+//     "marshmallow peeps"
+// ];
 
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-    [
-        // These are Thom's shirts
-        "grey button-up",
-        "dark grey button-up",
-        "light blue button-up",
-        "blue button-up",
-    ], [
-        // These are Thom's pants
-        "grey jeans",
-        "jeans",
-        "PJs"
-    ], [
-        // Thom's accessories
-        "wool mittens",
-        "wool scarf",
-        "raybans"
-    ]
-];
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//     [
+//         // These are Thom's shirts
+//         "grey button-up",
+//         "dark grey button-up",
+//         "light blue button-up",
+//         "blue button-up",
+//     ], [
+//         // These are Thom's pants
+//         "grey jeans",
+//         "jeans",
+//         "PJs"
+//     ], [
+//         // Thom's accessories
+//         "wool mittens",
+//         "wool scarf",
+//         "raybans"
+//     ]
+// ];
 
 //1.What's Kristyn wearing today?
 //  Using bracket notation to access items in kristynsCloset,
 //  log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
 
 
-console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
+// console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!")
 
 
 //2.Kristyn just bought some sweet shades! Add "raybans" to her closet after "yellow knit hat".
 
-kristynsCloset.splice(6, 0, "raybans")
-console.log(kristynsCloset);
+// kristynsCloset.splice(6, 0, "raybans")
+// console.log(kristynsCloset);
 
 //3.Kristyn spilled coffee on her hat... modify this item to read "stained knit hat" instead of yellow.
 
-kristynsCloset.splice(5, 1, "stained knit hat")
-console.log(kristynsCloset);
+// kristynsCloset.splice(5, 1, "stained knit hat")
+// console.log(kristynsCloset);
 
 //4. Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray.
 
-console.log(thomsCloset[0][0]);
+// console.log(thomsCloset[0][0]);
 
 //5. In the same way, access one item from Thom's pants array.
 
-console.log(thomsCloset[1][0]);
+// console.log(thomsCloset[1][0]);
 
 //6. Access one item from Thom's accessories array.
 
-console.log(thomsCloset[2][2]);
+// console.log(thomsCloset[2][2]);
 
 //7. Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!
 
-console.log("Thom is looking fierce in a " + thomsCloset[0][0] + "," + " " + thomsCloset[1][0] + " " + "and" + " " + thomsCloset[2][2] + "!");
+// console.log("Thom is looking fierce in a " + thomsCloset[0][0] + "," + " " + thomsCloset[1][0] + " " + "and" + " " + thomsCloset[2][2] + "!");
 
 //8. Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
 //need help could not figure this one out
 
-thomsCloset.splice(2, 0);
-console.log(thomsCloset);
+// thomsCloset.splice(2, 0);
+// console.log(thomsCloset);
+
+//FUNCTIONS
+//B printCool
+
+// const printCool = (agrument) => {
+//     return  agrument + " " + "is cool";
+// };
+// console.log(printCool("Captian Reynolds"));
+
+//C calculateCube
+
+// function calculateCube(number) {
+//     return number * number * number;
+// }
+
+// console.log(calculateCube(5));
+
+//D. isVowel
+
+function isVowel(charater) {
+    charater = charater.toLowerCase();
+
+
+    if (charater === 'a' || charater === 'e' || charater === 'i' || charater === 'o' || charater === 'u') {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isVowel('a'));
+console.log(isVowel('b'));
+console.log(isVowel('c'));
+console.log(isVowel('d'));
+console.log(isVowel('E'));
+
 

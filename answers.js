79d5cc330349743d701997f1555ec379ebe3f9a8
stackerd <danni2mar@gmail.com>
2024-07-09 23:02:06 -0400

    Yell at the Ninja Turtles answered

--- answers.js
+++ answers.js
@@ -26,33 +26,40 @@
 // }
 
 //Wild Wild Life
-const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
-const sharky = ["Sharky", "shark", 20, "Left Coast"];
-const plantee = ["Plantee", "plant", 5000, "Mordor"];
-const porgee = ["Porgee", "porg", 186, "Ahch-To"];
-const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
+// const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Demogorgan Dog", 2, "Upside Down"];
 
 // 1. Plantee just had her birthday, cahnge Plantee's array to reflect her beiag a year older.
-plantee[2] += 1;
+// plantee[2] += 1;
 
 //2. Change wolfy's hometown from "Yukon Territory" to "Gotham City".
 
-wolfy[3] = "Gotham City";
+// wolfy[3] = "Gotham City";
 
 //3.Give D'Art a second hometown by adding "Hawkins".
-dart.push("Hawkins");
+// dart.push("Hawkins");
 
 //4. Porgee decides that wolfy can't be named "Wolfy"anymore. Remove "Wolfy" from the wolf array and replace it with "Gameboy".
 
-wolfy[0] = "Gameboy";
+// wolfy[0] = "Gameboy";
 
-console.log(plantee);
-console.log(wolfy);
-console.log(dart);
-console.log(porgee);
-console.log(sharky);
+// console.log(plantee);
+// console.log(wolfy);
+// console.log(dart);
+// console.log(porgee);
+// console.log(sharky);
 
 // Yell at the Ninja Turtles
+
+const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+for (const turtle of ninjaTurtles) {
+    console.log(turtle.toUpperCase());
+}
+
 
 // Methods, Revisited
 
